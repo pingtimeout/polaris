9a7106588001978792f4b27e9aa4d2ff7cf24992
Pierre Laporte <pierre@pingtimeout.fr>
2025-02-27 10:49:16 +0100

    Update Gatling simulation to created duplicated tables

--- benchmarks/src/gatling/scala/IcebergRestSimulation.scala
+++ benchmarks/src/gatling/scala/IcebergRestSimulation.scala
@@ -15,6 +15,7 @@ class IcebergRestSimulation extends Simulation {
   // By default, we point to localhost
   private val baseUrl: String = sys.env.getOrElse("BASE_URL", "http://localhost:8181")
 
+  private val concurrency: Int = sys.env.getOrElse("CONCURRENCY", "50").toInt
 
   // --------------------------------------------------------------------------------
   // Feeders
@@ -25,17 +26,12 @@ class IcebergRestSimulation extends Simulation {
       "clientSecret" -> clientSecret
     )
   )
-  private val numCatalogs: Int = sys.env.getOrElse("NUM_CATALOGS", "1").toInt
-  private val catalogFeeder = Iterator.from(1).map { i =>
-    Map("catalogName" -> (s"Catalog"))
-  }
-  private val createdCatalogs = new AtomicInteger()
 
 
   // --------------------------------------------------------------------------------
-  // Workload: Authenticate and create catalogs
+  // Workload: Authenticate, then create catalog and namespace
   // --------------------------------------------------------------------------------
-  private val scn = scenario("Create catalogs using the Iceberg REST API")
+  private val createCatalogAndNamespace = scenario("Create catalog and namespace using the Iceberg REST API")
     // Authenticate using the /api/catalog/v1/oauth/tokens endpoint
     .tryMax(5) {
       exec(
@@ -54,32 +50,86 @@ class IcebergRestSimulation extends Simulation {
       )
     }
     // Then create the catalog
-    .doWhile(session => createdCatalogs.incrementAndGet() < numCatalogs && session.contains("accessToken"))(
-      feed(catalogFeeder)
-        .rendezVous(100)
-        .exec(
-          http("Create Catalog")
-            .post("/api/management/v1/catalogs")
-            .header("Authorization", "Bearer #{accessToken}")
-            .header("Content-Type", "application/json")
-            .body(
-              StringBody(
-                """{
-                  |  "catalog": {
-                  |    "type": "INTERNAL",
-                  |    "name": "#{catalogName}",
-                  |    "properties": {
-                  |      "default-base-location": "file:///tmp/polaris2"
-                  |    },
-                  |    "storageConfigInfo": {
-                  |      "storageType": "FILE"
-                  |    }
-                  |  }
-                  |}""".stripMargin
-              )
-            )
-            .check(status.is(201))
-        ))
+    .exec(
+      http("Create Catalog")
+        .post("/api/management/v1/catalogs")
+        .header("Authorization", "Bearer #{accessToken}")
+        .header("Content-Type", "application/json")
+        .body(
+          StringBody(
+            """{
+              |  "catalog": {
+              |    "type": "INTERNAL",
+              |    "name": "C_0",
+              |    "properties": {
+              |      "default-base-location": "file:///tmp/polaris2"
+              |    },
+              |    "storageConfigInfo": {
+              |      "storageType": "FILE"
+              |    }
+              |  }
+              |}""".stripMargin
+          )
+        )
+        .check(status.is(201))
+    )
+    // Then create the namespace
+    .exec(
+      http("Create Namespace")
+        .post("/api/catalog/v1/C_0/namespaces")
+        .header("Authorization", "Bearer #{accessToken}")
+        .header("Content-Type", "application/json")
+        .body(
+          StringBody(
+            """{
+              |  "namespace": ["NS_0"]
+              |}""".stripMargin
+          )
+        )
+        .check(status.is(200))
+    )
+
+  // --------------------------------------------------------------------------------
+  // Workload: Authenticate, then create tables
+  // --------------------------------------------------------------------------------
+  private val createTables = scenario("Create tables using the Iceberg REST API")
+    // Authenticate using the /api/catalog/v1/oauth/tokens endpoint
+    .tryMax(5) {
+      exec(
+        feed(authenticationFeeder)
+          .exec(
+            http("Authenticate")
+              .post("/api/catalog/v1/oauth/tokens")
+              .header("Content-Type", "application/x-www-form-urlencoded")
+              .formParam("grant_type", "client_credentials")
+              .formParam("client_id", "#{clientId}")
+              .formParam("client_secret", "#{clientSecret}")
+              .formParam("scope", "PRINCIPAL_ROLE:ALL")
+              .check(status.is(200))
+              .check(jsonPath("$.access_token").saveAs("accessToken"))
+          )
+      )
+    }
+    .rendezVous(concurrency)
+    .exec(
+      http("Create Table")
+        .post("/api/catalog/v1/C_0/namespaces/NS_0/tables")
+        .header("Authorization", "Bearer #{accessToken}")
+        .header("Content-Type", "application/json")
+        .body(
+          StringBody(
+            s"""{
+               |  "name": "T_1000",
+               |  "stage-create": false,
+               |  "schema": {
+               |    "type": "struct",
+               |    "fields": [{ "id": 0, "name": "column0", "type": "int", "required": true }],
+               |    "identifier-field-ids": [0]
+               |  }
+               |}""".stripMargin
+          )
+        )
+    )
 
   // --------------------------------------------------------------------------------
   // Build up the HTTP protocol configuration and set up the simulation
@@ -90,8 +140,7 @@ class IcebergRestSimulation extends Simulation {
     .contentTypeHeader("application/json")
 
   setUp(
-    scn.inject(
-      atOnceUsers(200)
-    )
+    createCatalogAndNamespace.inject(atOnceUsers(1))
+      .andThen(createTables.inject(atOnceUsers(concurrency)))
   ).protocols(httpProtocol)
 }
